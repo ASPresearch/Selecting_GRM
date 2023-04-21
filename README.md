0efced9d9562b2dfd14f4df2a5ee35121bc79824
alexsanchezpla <asanchez@ub.edu>
2023-04-21 08:12:02 +0200

    Cleaning text and improving format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Selecting_GRM
-Selection of genes with L pattern in the expression-methylation scatterplot, potentially regulated by methylation.
+# Selecting Genes Regulated by Methylation with Lheuristic package
+
+This document reviews basic concepts and provides examples of use of the methodology developed by Sanchez et alt to select genes potentially regulated by methylation, and characterized by an L-shape scatterplot, using an heuristic algorithm, implemented in the R `Lheuristic`package.
--- convertRnw2Rmd.R
+++ convertRnw2Rmd.R
@@ -2,7 +2,7 @@ curDir<- getwd()
 library(Hmisc)
 getRs('convertL2M.r', put='source')   # gets from github
 # setwd("/media/alex/Seagate Expansion Drive/Treballs/2011-Materials en elaboracio per assignatura ANALISI DE DADES D'ALT RENDIMENT/1.1. Introduccio Als microarrays-c1")
-sourceFileName <- "AnalysisReport-1_HeuristicMethod.Rnw"
-destFileName <- "AnalysisReport-1_HeuristicMethod.Rmd"
+sourceFileName <- "Selecting_GRM_with_Lheuristic.Rnw"
+destFileName <- "Selecting_GRM_with_Lheuristic.Rmd"
 convertL2M(sourceFileName, destFileName)
 setwd(curDir)
